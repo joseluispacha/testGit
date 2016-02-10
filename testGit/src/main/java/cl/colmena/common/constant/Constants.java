06479fdce2fba07ce935932c069175b60bb4280a
Jose Menendez <joseluispacha@gmail.com>
2016-02-10 16:30:52 -0300

    Se deja el ID de proyecto Original

--- testGit/src/main/java/cl/colmena/common/constant/Constants.java
+++ testGit/src/main/java/cl/colmena/common/constant/Constants.java
@@ -3,7 +3,7 @@ package cl.colmena.common.constant;
 public class Constants {
 	public static String SESSION_USUARIO = "SESSION_USUARIO";	
 	public static String SESSION_EXPIRADA = "SESSION_EXPIRADA";	
-	public static Integer ID_APP = 206;
+	public static Integer ID_APP = 166;
 	public static String VERSION_APP = "2.0-MAVEN";
 
 }
